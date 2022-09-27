e2d3f16f72a1b21c931960326647aa44ace7b404
Daeho Lee <lavr2755@gmail.com>
2022-09-27 19:17:53 +0900

    ttt

---
+++ src/main/java/fatdb/fatdbback/FatdbBackApplication.java
@@ -0,0 +1,13 @@
+package fatdb.fatdbback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatdbBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatdbBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatdb/fatdbback/FatdbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fatdb.fatdbback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatdbBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
